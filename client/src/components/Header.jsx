c24c4866c8eae09861deb91bd8c36743695c4513
ChemzRegaton <112966149+Jedlouies@users.noreply.github.com>
2025-10-15 14:13:27 +0800

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import './styles/Header.css'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles/Header.css';
+
+const isDesktopMode = () => {
+  const userAgent = navigator.userAgent;
+
+  const looksLikeDesktop = !/Mobi|Android|iPhone/i.test(userAgent);
+  const isSmallScreen = window.screen.width < 768; // Common breakpoint for tablets
+
+  return looksLikeDesktop && isSmallScreen;
+};
 
 function Header() {
   const navigate = useNavigate();
+  const [isDesktopView, setIsDesktopView] = useState(false);
+
+  useEffect(() => {
+    const desktopModeOn = isDesktopMode();
+    setIsDesktopView(desktopModeOn);
+    console.log("Is 'Desktop site' toggled on?", desktopModeOn); // For testing
+  }, []);
 
   const signInClicked = () => {
     navigate('/sign-in');
@@ -11,10 +27,16 @@ function Header() {
 
   return (
     <div className='header-container'>
-        <img src="/spotsync-slogan.png" alt="img" height={'50px'}/>
-        <button onClick={signInClicked}>Sign Up</button>
+      <img src="/spotsync-slogan.png" alt="img" height={'50px'} />
+      {/* 4. Conditionally apply the style to the button */}
+      <button
+        onClick={signInClicked}
+        style={isDesktopView ? { opacity: 0, pointerEvents: 'none' } : {}}
+      >
+        Sign Up
+      </button>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
